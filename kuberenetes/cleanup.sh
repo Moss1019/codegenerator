c9b403c3fab3f40ff20d221439297dc9d9898f3b
Moss1019 <mossonthetree@gmail.com>
2022-04-04 19:24:37 +0200

    ads

--- kuberenetes/cleanup.sh
+++ kuberenetes/cleanup.sh
@@ -1,9 +1,2 @@
-kubectl delete -n codegen-ns ingress codegen
-
-kubectl delete -n codegen-ns svc codegenweb
-kubectl delete -n codegen-ns svc codegen
-
-kubectl delete -n codegen-ns rs codegenweb
-kubectl delete -n codegen-ns rs codegen
 
 kubectl delete ns codegen-ns
